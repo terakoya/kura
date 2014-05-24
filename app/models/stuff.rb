965fe455602dcbec9d22c6da667c89c6de7ab100
Shota Fukumori (sora_h) <her@sorah.jp>
2014-05-24 16:09:26 +0900

    Stuff#file=: save uploaded file into files_dir
       p uploaded_file.original_filename #=> 'a.jpg'
       stuff.file = uploaded_file
       p stuff.filename #=> '20140524-161100-deadbeef.jpg'
       p File.exist?(stuff.filepath) #=> true

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -1,2 +1,24 @@
 class Stuff < ActiveRecord::Base
+  validates :filename, presence: true
+
+  def file
+  end
+
+  def file=(file)
+    self.filename = create_filename(file)
+    File.binwrite self.filepath, file.read
+  end
+
+  def filepath
+    File.join(Rails.application.secrets.files_dir,
+              File.basename(self.filename.to_s))
+  end
+
+  private
+
+  def create_filename(file)
+    ext = File.extname(file.original_filename)
+    "#{Time.now.strftime("%Y%m%d-%H%M%S")}-" \
+    "#{rand(1000000).to_s(16)}#{ext}"
+  end
 end
---
+++ spec/models/stuff_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe Stuff, :type => :model do
+  subject(:stuff) { Stuff.new(title: 'foo') }
+
+  describe "#file=" do
+    let(:uploaded_file) { double('uploaded_file') }
+    let(:files_dir) { Rails.application.secrets.files_dir }
+
+    before do
+      allow(uploaded_file).to receive(:read).and_return("content\n")
+      allow(uploaded_file).to receive(:original_filename).
+        and_return('foo.jpg')
+    end
+
+    it "saves UploadedFile into secrets.files_dir" do
+      stuff.file = uploaded_file
+      stuff.save!
+
+      expect(stuff.filename).not_to be_nil
+      path = File.join(files_dir, stuff.filename)
+      expect(File.read(path)).to eq "content\n"
+    end
+  end
+
+  describe "#filepath" do
+    before do
+      stuff.filename = 'a.jpg'
+      allow(Rails.application.secrets).to receive(:files_dir).
+        and_return('/tmp')
+    end
+
+    it "returns absolute path where the file exists" do
+      expect( stuff.filepath ).to eq '/tmp/a.jpg'
+    end
+  end
+end
